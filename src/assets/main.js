d736d26ee20fbb853ccda50ebf4db371cc05d476
fodor.daniel <fodor.daniel@w5.hu>
2016-12-11 11:59:28 +0100

    ajax v2

--- src/assets/main.js
+++ src/assets/main.js
@@ -12,7 +12,9 @@ $(function() {
         });
 
         var h3 = $("<h3 />").text(courses[i].title);
-        var img = $("<img />").text(courses[i].badge);
+        var img = $("<img />", {
+          src: courses[i].badge
+        });
         var button = $("<a />", {
           href: courses[i].href,
           target: "_blank",
