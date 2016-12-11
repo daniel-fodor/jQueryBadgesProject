6ed8094c1393e54998532be5db4546ae5fd57869
fodor.daniel <fodor.daniel@w5.hu>
2016-12-11 11:23:35 +0100

    Get my courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,29 @@
 $(function() {
 
   // your code will go here
+  $.ajax {
+    url: "https://www.codeschool.com/users/2436579.json",
+    dataType: 'jsonp',
+    success: function(response) {
+      var courses = response.courses.completed;
+      for (var i = 0; i < courses.length; i++) {
+        var div = $("<div />", {
+          class: 'course'
+        });
 
+        var h3 = $("<h3 />").text(courses.title);
+        var img = $("<img />").text(courses.badge);
+        var button = $("<a />",{
+          href: courses.href,
+          target: "_blank",
+          class: "btn, btn-primary"
+        }).text("See Course");
+
+        $(div).append(h3);
+        $(div).append(img);
+        $(div).append(button);
+        $("#badges").append(div);
+      }
+    }
+  }
 });
